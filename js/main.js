09b7580b9a7eb5ac1000b0da9a7cdacaa9a461b0
GerardoRiccio <dainok90@gmail.com>
2024-05-13 23:25:16 +0200

    edit: complete

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@
     const firstName = prompt('Inserisci il tuo nome');
     const lastName = prompt('Inserisci il tuo cognome');
     const colorFavorite = prompt('Inserisci il tuo colore preferito');
+//////////////////// INIZIO EXTRA aggiungi 2 numeri e dividi il primo per il secondo////////////////////
+    let firstNumber = prompt('Ti chiederemo 2 numeri diversi (il primo deve essere maggiore del secondo) Inserisci il primo numero');
+    firstNumber = parseInt(firstNumber);
+    let secondNumber = prompt('Inserisci il secondo numero'); 
+    secondNumber = parseInt(secondNumber);
+    let numberComplete = firstNumber / secondNumber;  
+//////////////////// FINE EXTRA aggiungi 2 numeri e dividi il primo per il secondo////////////////////
 // FINE - Richiesta all'utente tramite prompt (Prendo i valori in input e li salvo nelle variabili)
+
 
 // INIZIO - Creo una variabile che contiene i dati formattati in maniera specifica
     const output = `
     --------- (insicurissimo) password generator ---------- 
 
     password: ${firstName}${lastName}${colorFavorite}21
+    password Extra: ${firstName}${lastName}${colorFavorite}${numberComplete}
+
     
     ---------------------------------------- 
     Contattami per maggiori informazioni
@@ -23,6 +33,11 @@
     document.getElementById('nome-utente').innerHTML = firstName;
     document.getElementById('cognome-utente').innerHTML = lastName;
     document.getElementById('colorePreferito-utente').innerHTML = colorFavorite;
+//////////////////// INIZIO EXTRA Mostrare la password generata nel Layout HTML////////////////////
+    document.getElementById('passwordDefault').innerHTML =`${firstName}${lastName}${colorFavorite}21`;
+    document.getElementById('passwordExtra').innerHTML =`${firstName}${lastName}${colorFavorite}${numberComplete}`;
+
+//////////////////// FINE EXTRA Mostrare la password generata nel Layout HTML////////////////////
 // FINE (NON RICHIESTO) - modificare il contehnuto HTML
 
 
@@ -31,9 +46,6 @@
 //documentati su come trasformare una stringa in un numero
 
 
-
-const firstNumber = prompt('Ti chiederemo 2 numeri diversi (il primo deve essere maggiore del secondo) Inserisci il primo numero');
-const secondNumber = prompt('Inserisci il secondo numero');
 
 
 
